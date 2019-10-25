5296c7dc18f5d750d8db43c10dcff5da9bede0e5
hwakabh <hrykwkbys1024@gmail.com>
2019-10-25 20:11:08 +0900

    Added base structures of parent scripts.

---
+++ PowerShell/kick_vm_restart_remotely/parentScript.ps1
@@ -0,0 +1,36 @@
+$childFileName = "childScript.ps1"
+$childDirPath = "C:\Users\Administrator"
+$primaryChildIp = "172.16.110.29"
+$secondaryChildIp = "172.16.110.248"
+
+
+function testConnection ($target) {
+    Write-Host "WIP : testing L4 connction."
+    # with TestNetConnection cmdlet
+    # return: Boolean value whether it could be connected or not
+}
+
+function callChild ($filepath) {
+    Write-Host "WIP : kicking childScript remotely..."
+    # with Invoke-Command cmdlet
+    # TODO: getReturn codes and return to main function
+}
+
+
+if ($args.Length -eq 0) {
+    Write-Host "ERROR : Missing Arguments."
+} elseif ($args.Length -eq 1) {
+    Write-Host "INFO Starting to operation..."
+    if (testConnection($primaryChildIPp)) {
+        Write-Host "INFO : Kick script on rimary childScript ..."
+        # sys.exit()
+    elseif (testConnection($secondaryChildIp)) {
+        Write-Host "WARNING : Kick script on secondary childScript..."
+        # sys.exit()
+    else {
+        Write-Host "ERROR : Both of child unavailable..."
+        # Write eventvwr to ERROR message.
+        # sys.exit()
+} else {
+    Write-Host "ERROR : Too many arguments..."
+}
