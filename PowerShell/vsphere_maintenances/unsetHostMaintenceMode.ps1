897be93f9171caa061661378afc3bb53dc2d4d5f
hwakabh <hrykwkbys1024@gmail.com>
2019-10-30 16:33:33 +0900

    #19 Pushed unsetHostMaintenaceMode.ps1 based with setHostMaintenaceMode.ps1.

---
+++ PowerShell/vsphere_maintenances/unsetHostMaintenceMode.ps1
@@ -0,0 +1,27 @@
+# Pre-Requirements
+$targetClusters = $args
+$scriptRoot = "C:\Users\Administrator\Documents\misc-snippets\PowerShell\vsphere_maintenances"
+
+# Credentials
+$vCenter = "vcsa02.nfvlab.local"
+$username = "administrator@vsphere.local"
+$password = Get-Content $passwordFilePath | ConvertTo-SecureString
+$credential = New-Object -TypeName System.Management.Automation.PsCredential `
+    -ArgumentList $username, $password
+
+
+# Main functions
+try {
+    Connect-VIServer -Server $vCenter -Credentials $creds
+} catch {
+    Write-Host "Failed to connect vCenter Server [ $vCenter ]..."
+    exit 1
+}
+
+foreach ($cluster in $targetClsuters) {
+    # Retrieve Cluster name(s) as command line argument(s),
+    # and unset ESXi host(s) in the cluster to maintenance mode.
+    Get-Cluster -Name $cluster |Get-VMHost |Where-Object {$_.ConnectionState -eq "Maintenance"} |Set-VMHost -State "Connected"
+}
+
+exit 0
