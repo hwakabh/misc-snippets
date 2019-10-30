57b161191aed8c9736217a51962126ea4b87f879
hwakabh <hrykwkbys1024@gmail.com>
2019-10-31 05:23:23 +0900

    #21 Pushed snippets for creating new hostProfile with timestamps.

---
+++ PowerShell/vsphere_maintenances/getHostProfiles.ps1
@@ -0,0 +1,30 @@
+# Pre-Requirements
+$targetClusters = $args
+$scriptRoot = "C:\Users\Administrator\Documents\misc-snippets\PowerShell\vsphere_maintenances"
+
+# Credentials
+$vCenter = "vcsa02.nfvlab.local"
+$username = "administrator@vsphere.local"
+$password = Get-Content $passwordFilePath | ConvertTo-SecureString
+$credential = New-Object -TypeName System.Management.Automation.PsCredential `
+    -ArgumentList $username, $password
+
+
+# Main functions
+try {
+    Connect-VIServer -Server $vCenter -Credentials $creds
+} catch {
+    Write-Host "Failed to connect vCenter Server [ $vCenter ]..."
+    Disconnect-VIServer -Server $vCenter
+    exit 1
+}
+
+foreach ($cluster in $targetClsuters) {
+    $esxihosts = Get-Cluster -Name $cluster
+    foreach ($h in $esxihosts) {
+        $profileName = "$h_$(Get-Date -Format "yyyyMMdd_HHmmss")"
+        New-VMHostProfile -Name $profileName -ReferenceHost $h
+    }
+}
+
+exit 0
