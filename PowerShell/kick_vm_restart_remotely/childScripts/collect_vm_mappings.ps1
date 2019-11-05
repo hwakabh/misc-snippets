8c7f61a718396db9f715a9af08e980265eadfe32
hwakabh <hrykwkbys1024@gmail.com>
2019-11-05 21:44:17 +0900

    #33 Implemented utilities to export virtual machine mappings.

---
+++ PowerShell/kick_vm_restart_remotely/childScripts/collect_vm_mappings.ps1
@@ -0,0 +1,36 @@
+# NOTE: this scripts pre-requires password.secret file generated with Get-Credential cmdlet
+$scriptPath = Convert-Path ..\
+$passwordFilePath = Join-Path -Path $scriptPath -ChildPath "password.secret"
+# Event source name is same as script filename itself
+$eventSrcName = $PSCommandPath.Split('\')[-1]
+
+$vCenter = "vcsa01.nfvlab.local"
+$username = "administrator@vsphere.local"
+$password = Get-Content $passwordFilePath | ConvertTo-SecureString
+$credential = New-Object -TypeName System.Management.Automation.PsCredential `
+    -ArgumentList $username, $password
+
+$outFileName = "result.csv"
+
+try {
+    Connect-VIServer -Server $vCenter -Credential $credential |Out-Null
+    Write-Output "Successfully connected vCenter Server [ $vCenter ]"
+} catch {
+    Write-Output "Failed to connect vCenter Server [ $vCenter ]. Exit the program."
+    exit 128
+}
+
+# Collecting relations and export them to csv.
+Write-Output "Dumping relations with virtual-machines..."
+&{foreach($dc in Get-Datacenter){
+    foreach($cluster in Get-Cluster -Location $dc){
+        foreach($esx in Get-VMHost -Location $cluster){
+            Get-VM -Location $esx |
+            Select @{N='Datacenter';E={$dc.Name}},
+                @{N='Cluster';E={$cluster.Name}},
+                @{N='VMhost';E={$esx.Name}},Name
+        }
+    }
+}} | Export-Csv $outFileName -NoTypeInformation -UseCulture
+Write-Output "Successfully exported to csv file [ $outFileName ]."
+exit 0
