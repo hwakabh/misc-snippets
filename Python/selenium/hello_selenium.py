65846f9bd740e3b1287080b50bd8774c1442c4de
hwakabh <hrykwkbys1024@gmail.com>
2020-05-02 00:54:24 +0900

    Added descruction methods for driver instances.

--- Python/selenium/hello_selenium.py
+++ Python/selenium/hello_selenium.py
@@ -3,3 +3,6 @@ from selenium import webdriver
 # Instanciate with path to chromedriver binary
 driver = webdriver.Chrome(executable_path='/Users/hwakabayashi/chromedriver')
 driver.get('https://google.co.jp')
+
+print('>>> Opening Chrome driver done.')
+driver.quit()
