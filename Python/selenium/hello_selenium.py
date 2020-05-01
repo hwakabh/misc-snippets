bd979788995691ccf167c098672fc0dbf6f478f4
hwakabh <hrykwkbys1024@gmail.com>
2020-05-01 17:02:04 +0900

    Added tutorial files for selenium chrome driver.

---
+++ Python/selenium/hello_selenium.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+# Instanciate with path to chromedriver binary
+driver = webdriver.Chrome(executable_path='/Users/hwakabayashi/chromedriver')
+driver.get('https://google.co.jp')
