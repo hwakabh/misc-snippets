4693432b097944d21e7b8af9cbcf9f085b1077ce
hwakabh <hrykwkbys1024@gmail.com>
2022-02-06 16:40:31 +0900

    Updated scripts for DeepL API and added some sample texts.

--- Python/scraping-tutorial/deepl_api_plays.py
+++ Python/scraping-tutorial/deepl_api_plays.py
@@ -1,34 +1,46 @@
 import os
+import sys
 import json
 import requests
 
 
 DEELP_TRANSLATE_URL = 'https://api.deepl.com/v2/translate'
 
-print('>>> Fetching API keys to use DeepL API ...')
+print('>>> Fetching API keys to use DeepL API ...\n')
 DEEPL_API_KEY = os.environ.get('DEEPL_API_KEY', None)
 if DEEPL_API_KEY is None:
     print('DEEPL_API_KEY not provides, please set environmental variable with your shell.')
+    sys.exit(1)
 
-srctext = '''
-こんにちは、私の名前はロバートです。DeepL の API を試しています。
-'''
 
-print('>>> Source text:')
-print(srctext)
+with open('./in_text/out_random_100.input.min') as f:
+    lines = f.readlines()
 
-print('>>> Starting translate with DeepL ...\n')
+print('>>> Number of publications: {}\n'.format(len(lines)))
 
-h = {'Content-Type': 'application/x-www-form-urlencoded'}
-body = {
-  'auth_key': DEEPL_API_KEY,
-  'text': srctext,
-  # 'source_lang': '',
-  'target_lang': 'EN',
-}
-res = requests.post(url=DEELP_TRANSLATE_URL, headers=h, data=body)
+for l in lines:
+    print('>>> ------------')
+    hsh = l.split('***')[0]
+    link_url = l.split('***')[1]
+    srctext = l.split('***')[2].replace('\\n', ' ').replace(' .', '')
 
-print('>>> Translated text:')
-trstext = json.loads(res.text)
-for t in trstext.get('translations', None):
-    print(t.get('text', None))
+    print(f'> Source URL: {link_url}')
+    print(f'> Glocalist URL: https://glocalist.asia/publications/#/{hsh}')
+    print('> Source text:')
+    print(srctext)
+
+    print('> Starting translate with DeepL ...\n')
+
+    h = {'Content-Type': 'application/x-www-form-urlencoded'}
+    body = {
+      'auth_key': DEEPL_API_KEY,
+      'text': srctext,
+      'source_lang': 'EN',
+      'target_lang': 'JA',
+    }
+    res = requests.post(url=DEELP_TRANSLATE_URL, headers=h, data=body)
+
+    print('> Translated text:')
+    trstext = json.loads(res.text)
+    for t in trstext.get('translations', None):
+        print(t.get('text', None))
