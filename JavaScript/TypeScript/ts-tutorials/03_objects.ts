79f03c4d09632c0ce9fe2396272b0278fd8d88f7
hwakabh <hrykwkbys1024@gmail.com>
2024-02-14 23:06:49 +0900

    chore: cleanup.

--- JavaScript/TypeScript/ts-tutorials/03_objects.ts
+++
@@ -1,8 +0,0 @@
-let userProfile: object = {
-  "first_name": "Hiroyuki",
-  "last_name": "Wakabayashi",
-  "user_name": "hwakabh"
-}
-
-console.log(userProfile);
-console.log(userProfile["user_name"]);
